4711053d150c0cffa5037b017a2afbb193224a32
jibek12 <romanbekovazibek@gmail.com>
2023-10-28 20:02:02 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "zhibek.romanbekova_hw7_2m"
+include(":app")
+ 
